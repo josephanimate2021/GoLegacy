e2f2e4362c44456c1d0857a5f1e966f1cb4048aa
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-31 14:43:56 -0400

    Update load.js

--- asset/load.js
+++ asset/load.js
@@ -57,7 +57,6 @@ module.exports = function (req, res, url) {
 							case "localhost": 
 							case `localhost:${process.env.SERVER_PORT}`: {
 								loadPost(req, res).then(async ([data]) => {
-									console.log(data);
 									const ut = data.ut;
 									const aId = data.assetId || data.enc_asset_id;
 									asset.delete(ut, aId);
