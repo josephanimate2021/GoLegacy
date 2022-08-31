aa3f6d1e996982f2d745f3cf868eeb012ea3c4a1
josephanimate2021 <jackiecrosman@gmail.com>
2022-08-31 01:07:28 -0400

    removed verbose

--- waveform/core.js
+++ waveform/core.js
@@ -23,7 +23,6 @@ module.exports = function (req, res, url) {
 		case "/goapi/getWaveform/": {
 			loadPost(req, res).then(([data]) => {
 				if (!data.wftheme == "ugc") return;
-				console.log(data);
 				const wfFolder = `${folder}/${data.ut}.${data.wfid}.wf`;
 				const wfMp3Folder = `${folder}/${data.ut}.${data.wfid}`;
 				if (fs.existsSync(wfFolder)) res.end(fs.readFileSync(wfFolder));
