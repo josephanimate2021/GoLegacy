937331e1a13bba17bd74b643ddc2b015066e23bb
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-08-31 13:30:00 -0400

    Update main.js

--- asset/main.js
+++ asset/main.js
@@ -17,54 +17,27 @@ module.exports = {
 			case "prop": { 
 				suffix = `-${mode}.${ext}`;
 				return caché.newProp(buffer, ut, "", suffix); 
-                break;
-            }
-			case "wtr": { 
+				break;
+			} case "wtr": { 
 				suffix = `-${mode}.${ext}`;
 				return caché.newWatermark(buffer, ut, "", suffix, ext); 
 				break;
-            }
-			case "video": { 
-                suffix = `-${mode}.${ext}`;
-                if (mode == "dontimport") console.log;
+			} case "video": { 
+				suffix = `-${mode}.${ext}`;
+				if (mode == "dontimport") console.log;
 				else return caché.newVideo(buffer, ut, "", suffix); 
 				break;
-			}
-			default: {
+			} default: {
 				suffix = `-${mode}.${ext}`;
 				return caché.newItem(buffer, ut, "", suffix);
 				break;
-            }
+			}
 		}
 	},
 	list(ut, mode) {
 		var ret = [];
 		var files = caché.list(ut);
 		files.forEach((aId) => {
-			var dot = aId.lastIndexOf(".");
-			var dash = aId.lastIndexOf("-");
-			var name = aId.substr(0, dash);
-			var ext = aId.substr(dot + 1);
-			var fMode = aId.substr(dash + 1, dot - dash - 1);
-			switch (fMode) {	
-				case 'music':
-					var fMode = 'sound';
-					var subtype = 'bgmusic';
-					break;
-				case 'voiceover':
-					var fMode = 'sound';
-					var subtype = 'voiceover';
-					break;
-				case 'soundeffect':
-					var fMode = 'sound';
-					var subtype = 'soundeffect';
-					break;
-			}
-			if (fMode == mode) {
-				if (fMode == 'sound') ret.push({ id: aId, ext: ext, name: name, mode: fMode, subtype: subtype});
-				else ret.push({ id: aId, ext: ext, name: name, mode: fMode });	
-			}
-
 			return new Promise(function (resolve, reject) {
 				const buffer = fs.readFileSync(`${process.env.CACHÉ_FOLDER}/${ut}.${aId}`);
 				mp3Duration(buffer, (d) => {
@@ -74,8 +47,7 @@ module.exports = {
 					var name = aId.substr(0, dash);
 					var ext = aId.substr(dot + 1);
 					var subtype = aId.substr(dash + 1, dot - dash - 1);
-					if (!dur) ret.push({ id: aId, ext: ext, name: name, subtype: subtype});
-					else ret.push({ id: aId, ext: ext, name: name, subtype: subtype, duration: dur });
+					ret.push({ id: aId, ext: ext, name: name, subtype: subtype, duration: dur });
 				});
 				resolve(ret);
 				reject(ret)
