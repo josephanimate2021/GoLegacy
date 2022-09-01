81ec3454e9f5e014ec44c7e53c6d3a324c711f14
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-01 00:46:54 -0400

    Delete main.js

--- waveform/main.js
+++
@@ -1,7 +0,0 @@
-const folder = process.env.WAVEFORMS_FOLDER;
-const fs = require("fs");
-
-module.exports = function(wfId, waveform) {
-	var path = `${folder}/${wfId}.wf`;
-	fs.writeFileSync(path, waveform);
-};
