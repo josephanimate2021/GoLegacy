455b88b91f460de10c014b47ae09763941a012be
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-01 14:13:40 -0400

    Delete database.js

--- asset/database.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const folder = process.env.DATABASES_FOLDER;
-module.exports = {
-        load(aId) {
-                fs.readFileSync(`${folder}/meta-${aId}.json`);  
-        }
-}; 
