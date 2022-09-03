a50b798ec262799afe848275d3f259eed8feac21
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-03 15:02:57 -0400

    Update list.js

--- asset/list.js
+++ asset/list.js
@@ -9,10 +9,10 @@ const http = require("http");
 const fs = require("fs");
 function giveXml(type, v) {
 	const tempMeta = process.env.DATABASE_TEMP_FOLDER + `/starter-${v.id}.json`;
-	if (!fs.existsSync(tempMeta)) return;
 	const buffer = fs.readFileSync(tempMeta);
 	if (!fs.existsSync(process.env.DATABASES_FOLDER + `/starter-${v.id}.json`)) {
-		if (fs.existsSync(tempMeta)) {
+		if (!fs.existsSync(tempMeta)) return;
+		else {
 			fs.writeFileSync(process.env.DATABASES_FOLDER + `/starter-${v.id}.json`, buffer);
 			fs.unlinkSync(tempMeta);
 		}
