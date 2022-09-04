54aa83631521f016074467464bf06e909e5c6a8d
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-04 01:45:58 -0400

    Update upload.js

--- wrapper/character/upload.js
+++ wrapper/character/upload.js
@@ -3,8 +3,10 @@
  */
 // modules
 const fs = require("fs");
+const fUtil = require("../fileUtil");
 // stuff
 const Char = require("./main");
+const Movie = require("../movie/main");
 
 /**
  * @param {import("http").IncomingMessage} req
@@ -13,28 +15,47 @@ const Char = require("./main");
  * @returns {boolean}
  */
 module.exports = async function (req, res, url) {
-	if (req.method != "POST" || url.pathname != "/upload_character") return;
+	if (req.method != "POST") return;
+	switch (url.pathname) {
+		case "/upload_character": {
 
-	const path = req.files.import.filepath, buffer = fs.readFileSync(path);
-	const meta = {
-		type: "char",
-		subtype: 0,
-		title: "Untitled",
-		themeId: Char.getTheme(buffer)
-	};
-	try {
-		// save the char
-		Char.save(buffer, meta, true);
-		const url = `/cc_browser?themeId=${meta.themeId}`;
-		fs.unlinkSync(path);
-		// redirect the user
-		res.statusCode = 302;
-		res.setHeader("Location", url);
-		res.end();
-	} catch (err) {
-		console.error("Error uploading character:", err);
-		res.statusCode = 500;
-		res.end("00");
-	}
-	return true;
+			const path = req.files.import.filepath, buffer = fs.readFileSync(path);
+			const meta = {
+				type: "char",
+				subtype: 0,
+				title: "Untitled",
+				themeId: Char.getTheme(buffer)
+			};
+			try {
+				// save the char
+				Char.save(buffer, meta, true);
+				const url = `/cc_browser?themeId=${meta.themeId}`;
+				fs.unlinkSync(path);
+				// redirect the user
+				res.statusCode = 302;
+				res.setHeader("Location", url);
+				res.end();
+			} catch (err) {
+				console.error("Error uploading character:", err);
+				res.statusCode = 500;
+				res.end("00");
+			}
+			return true;
+		} case "/upload_movie": {
+			const path = req.files.import.filepath, buffer = fs.readFileSync(path);
+			const id = fUtil.generateId();
+			try {
+				fs.writeFileSync(process.env.MOVIES_FOLDER + `${id}.xml`, buffer);
+				Movie.createThumb(id);
+				const url = `/go_full?movieId=${id}`;
+				res.statusCode = 302;
+				res.setHeader("Location", url);
+				res.end();
+			} catch (e) {
+				console.error("Error uploading movie:", e);
+				res.statusCode = 500;
+				res.end();
+			}
+		}
+	}			
 }
