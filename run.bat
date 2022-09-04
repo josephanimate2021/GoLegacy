574b2c80cbf15fb141a692c49f7e2ca72014aab0
josephanimate2021 <jackiecrosman@gmail.com>
2022-09-03 21:28:29 -0400

    api 1 is deleted for api 2

--- run.bat
+++
@@ -1,9 +0,0 @@
-:: Installation
-@echo off && cls
-taskkill /f /im node.exe
-if not exist node_modules ( npm install &&  npm start ) else ( 
-        :: start golegacy
-        npm start
-        pause
-)
-
--- theme/list.js
+++
@@ -1,8 +0,0 @@
-const fUtil = require('../misc/file');
-const folder = process.env.THEME_FOLDER;
-module.exports = function (req, res, url) {
-	if (req.method != 'POST' || url.path != "/goapi/getThemeList/") return;
-	res.setHeader('Content-Type', 'application/zip');
-	fUtil.makeZip(`${folder}/themelist.xml`, 'themelist.xml').then(b => res.end(b)).catch(e => console.log("Error:", e));
-	return true;
-}
